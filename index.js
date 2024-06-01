1c5dbca45f8275ab16647c6cfa7f59d726f7a4eb
Chris Cassano <chris@litprotocol.com>
2024-05-31 19:16:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const { ethers } = require("ethers");
+
+const { getSigner, getNetworkInfo } = require("./utils.js");
+
+const pkpHelperABI = require("./abis/PKPHelper.json");
+const pkpABI = require("./abis/PKPNFT.json");
+
+const litNetwork = "manzano";
+const howManyToMint = 10;
+
+async function main() {
+  const network = await getNetworkInfo(litNetwork);
+  const { pkpHelperContractAddress, pkpNftContractAddress, rpcUrl } = network;
+
+  const signer = await getSigner(rpcUrl);
+  const signerAddress = await signer.getAddress();
+
+  const pkpHelper = new ethers.Contract(
+    pkpHelperContractAddress,
+    pkpHelperABI,
+    signer
+  );
+  const pkp = new ethers.Contract(pkpNftContractAddress, pkpABI, signer);
+
+  const allTimings = [];
+
+  console.log("getting mint cost...");
+  const mintCost = await pkp.mintCost();
+  console.log("mintCost is ", mintCost.toString());
+  for (let i = 0; i < howManyToMint; i++) {
+    console.log(`minting pubkey ${i}`);
+    const timings = {};
+    try {
+      // get txn count
+      // let now = Date.now();
+      // const nonce = await signer.getTransactionCount();
+      // let elapsed = Date.now() - now;
+      // timings["getNonce"] = elapsed;
+      // mint and let the signerAddress use it
+      let now = Date.now();
+      const mintTx = await pkpHelper.mintNextAndAddAuthMethods(
+        2,
+        [1],
+        [signerAddress],
+        ["0x00"],
+        [[1]],
+        false,
+        false,
+        { value: mintCost }
+      );
+      let elapsed = Date.now() - now;
+      timings["sendTx"] = elapsed;
+      console.log("mintTx hash", mintTx.hash);
+      now = Date.now();
+      const receipt = await mintTx.wait();
+      elapsed = Date.now() - now;
+      timings["waitForTxConfirmation"] = elapsed;
+      const publicKey = receipt.logs[2].data.substr(130, 130);
+      const tokenId = ethers.keccak256("0x" + publicKey);
+      console.log(
+        `Minting took ${elapsed}ms to confirm.  Checking with local chronicle node to see if we got ${publicKey} yet`
+      );
+      console.log(
+        `Success on number ${i}: ${JSON.stringify(timings, null, 2)}`
+      );
+      allTimings.push(timings);
+    } catch (e) {
+      console.log(`error minting and signing with pkp ${i}`, e);
+    }
+  }
+
+  console.log(`All timings: ${JSON.stringify(allTimings, null, 2)}`);
+  console.log("Success!");
+  process.exit(0);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.log("main error handler triggered.  error below:");
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ utils.js
@@ -0,0 +1,100 @@
+const { ethers } = require("ethers");
+const { ethers: ethersv5 } = require("ethers-v5");
+const stakingContractABI = require("./abis/Staking.json");
+const resolverContractABI = require("./abis/ContractResolver.json");
+const fs = require("fs");
+
+const rpcUrl = "https://lit-protocol.calderachain.xyz/replica-http";
+const privateKey = process.env.LIT_PKP_PARALLEL_KEYGEN_PRIVATE_KEY;
+
+const networks = {
+  manzano: {
+    stakingContractAddress: "0xBC7F8d7864002b6629Ab49781D5199C8dD1DDcE1",
+    env: "dev",
+  },
+};
+
+const getSigner = async (rpcUrl) => {
+  const provider = new ethers.JsonRpcProvider(rpcUrl);
+  const wallet = new ethers.Wallet(privateKey, provider);
+  return wallet;
+};
+
+const envToEnum = (env) => {
+  switch (env) {
+    case "dev":
+      return 0;
+    case "staging":
+      return 1;
+    case "prod":
+      return 2;
+    default:
+      throw new Error("ENV is invalid");
+  }
+};
+
+const getNetworkInfo = async (selectedNetwork) => {
+  console.log("Resolving network info from chain, 1 sec...");
+
+  // resolve addresses
+  const network = networks[selectedNetwork];
+  const { env, stakingContractAddress } = network;
+
+  // lookup resolver contract address from staking contract
+  const provider = new ethers.JsonRpcProvider(rpcUrl);
+
+  const stakingContract = new ethers.Contract(
+    stakingContractAddress,
+    stakingContractABI,
+    provider
+  );
+
+  const resolverContractAddress = await stakingContract.contractResolver();
+
+  const resolverContract = new ethers.Contract(
+    resolverContractAddress,
+    resolverContractABI,
+    provider
+  );
+
+  const contractContext = {
+    resolverAddress: resolverContractAddress,
+    abi: resolverContractABI,
+    environment: envToEnum(env),
+    provider: new ethersv5.providers.JsonRpcProvider(rpcUrl),
+  };
+
+  const networkInfo = {
+    rpcUrl,
+    stakingContractAddress,
+    resolverContractAddress,
+    env: network.env,
+    contractContext,
+  };
+
+  networkInfo.pkpNftContractAddress = await resolverContract.getContract(
+    await resolverContract.PKP_NFT_CONTRACT(),
+    envToEnum(network.env)
+  );
+
+  networkInfo.pkpHelperContractAddress = await resolverContract.getContract(
+    await resolverContract.PKP_HELPER_CONTRACT(),
+    envToEnum(network.env)
+  );
+
+  networkInfo.pubkeyRouterContractAddress = await resolverContract.getContract(
+    await resolverContract.PUB_KEY_ROUTER_CONTRACT(),
+    envToEnum(network.env)
+  );
+
+  networkInfo.rateLimitNftAddress = await resolverContract.getContract(
+    await resolverContract.RATE_LIMIT_NFT_CONTRACT(),
+    envToEnum(network.env)
+  );
+  return networkInfo;
+};
+
+module.exports = {
+  getNetworkInfo,
+  getSigner,
+};
